e4b1af6768878b03d12d8fe91fcf072c1c2636cc
Salim Hassouna <hassounasalim842@gmail.com>
2025-04-18 02:32:58 +0200

    feat: define Express routes for catmash api and link them to controller actions

---
+++ src/routes/cats.routes.js
@@ -0,0 +1,19 @@
+// Import Express
+const express = require("express");
+
+// Create a new router
+const router = express.Router();
+
+// Import the cats controller
+const controller = require("../controllers/cats.controller.js");
+
+// Define routes and link them to controller functions
+router.get("/", controller.showHomePage);
+router.get("/cats", controller.fetchAllCats);
+router.get("/cats/count", controller.fetchCatsCount);
+router.get("/cats/random", controller.fetchRandomCats);
+router.get("/cats/:id", controller.fetchCatById);
+router.put("/cats/:id/score", controller.updateCatScore);
+
+// Export the router
+module.exports = router;
