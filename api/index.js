5c3f34ecc893689006a05aa1c9322cfc103e5295
Salim Hassouna <hassounasalim842@gmail.com>
2025-04-19 02:15:52 +0200

    fix: remove promise handling

--- api/index.js
+++ api/index.js
@@ -15,26 +15,22 @@ const syncImagesToDatabase = require("../src/db/database");
 const url = `${process.env.API_BASE_URL}/data/cats.json`;
 
 // Fetch images and save them to the database
-syncImagesToDatabase(url)
-  .then(() => {
-    // Create Express app
-    const app = express();
+syncImagesToDatabase(url);
 
-    // Middleware
-    app.use(cors());
-    app.use(express.json());
+// Create Express app
+const app = express();
 
-    // Routes
-    app.use("/", catsRoutes);
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-    // Define the port
-    const port = process.env.PORT || 5000;
+// Routes
+app.use("/", catsRoutes);
 
-    // Start the server
-    app.listen(port, () => {
-      console.log(`Server is running at port : ${port}`);
-    });
-  })
-  .catch((err) => {
-    console.log("error in the server");
-  });
+// Define the port
+const port = process.env.PORT || 5000;
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at port : ${port}`);
+});
